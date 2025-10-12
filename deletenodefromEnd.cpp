ebe37c7ce3dd3f300688fd5debe0f6ff8c1753a6
GaoRongrong <2786451459@qq.com>
2025-10-12 16:27:09 +0800

    2025.10.11 阴 删除倒数第n个节点 双指针法

---
+++ deletenodefromEnd.cpp
@@ -0,0 +1,31 @@
+class Solution{
+public:
+
+    struct ListNode{
+        int val;
+        ListNode* next;
+        ListNode(int x):val=xo,ListNode=NULL{};
+    }
+    ListNode* removementNthFromEnd(ListNode* head,int){
+        ListNode* dummyHead=new ListNode(0);
+         dummyHead->next = head;
+        ListNode* slow = dummyHead;
+        ListNode* fast = dummyHead;
+        while(n-- && fast != NULL) {
+            fast = fast->next;
+        }
+        fast = fast->next; // fast再提前走一步，因为需要让slow指向删除节点的上一个节点
+        while (fast != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next; 
+        
+        // ListNode *tmp = slow->next;  C++释放内存的逻辑
+        // slow->next = tmp->next;
+        // delete tmp;
+        
+        return dummyHead->next;
+    
+    }
+};
