e21eaf9b774af423be6444d9ba1eb0ec5338cf51
GaoRongrong <2786451459@qq.com>
2025-11-09 14:13:35 +0800

    2025.11.09 阴 感冒 反转字符串中的单词

--- reverseWords.cpp
+++ reverseWords.cpp
@@ -1,17 +1,35 @@
 class Solution{
-public:
-    void reverse(string& s ,int start ,int end){
-        for(int i=start,j=end;i<j;i++,j--){
-            swap(s[i],s[j]);
+    pubilc:
+        void reverse(string& s,int start,int end){
+            for(int i=start,j=end;i<j;i++,j--){
+                __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1(s[i],s[j]);
+            }
         }
+
+    void removeExtraSpaces(string& s){
+        int slow=0;
+        for(int i=0;i<s.size();++i){
+            if(s[i]!=' '){
+                if(slow!=0)s[slow++]=' ';
+                while(i<s.size()&&s[i]!=' '){
+                    s[slow++]=s[i++];
+                }
+            }
+        }
+        s.resize(slow);
     }
-void removeExtraSpace(string& s){
-    int slow=0;
-    for(int i=0;i<s.size();i++){
-        if(s[i]!=''){
-            if(slow!=0) s[slow++] = ' ';
+
+    string reverseWords(string s ){
+        removeExyraSpace(s);//除去多余空格
+        reverse(s,0,s.size(-1));//反转字符串
+        int start=0;
+        for(int i=0li<=s.size();++i){
+            if(i==s.size()||s[i]==' '){
+                reverse(s,start,i-1);
+                start=i+1;
+            }
         }
+        return s;
+
     }
 }
-
-} 
